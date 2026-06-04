a66498a8f2073ca8f735e3f7e340b7dba9467a62
Your Name <you@example.com>
2026-06-04 11:43:15 +0700

    hehehe

--- public/js/demo/datatables-demo.js
+++
@@ -1,9 +0,0 @@
-// Call the dataTables jQuery plugin
-$(document).ready(function () {
-  $('#dataTable').DataTable({
-    ordering: false,
-    language: {
-      url: "/js/lang/vi.json"
-    }
-  });
-});
